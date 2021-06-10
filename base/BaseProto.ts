1ffcf3549e4f43af796e05a5e892c7109d378803
rolldone <donny.rollproject@gmail.com>
2021-06-10 08:37:57 +0800

    - Big improvement, Improvement sftp upload and download - Watcher now have multi path

--- base/BaseProto.ts
+++ base/BaseProto.ts
@@ -8,5 +8,48 @@ export default UberProto.extend<BaseProtoInterface<any>>({
     return input.slice(0, start)
         + input.slice(start, end).replace(search, replace)
         + input.slice(end);
+  },
+  _getStatInfo : function(permission,passTypeData){
+    let infoCollections : Array<string> = [
+      '0010000', // named pipe (fifo)
+      '0020000', // character special
+      '0040000', // directory
+      '0060000', // block special
+      '0100000', // regular
+      '0120000', // symbolic link
+      '0140000', // socket
+      '0160000' //whiteout 
+    ];
+    let whatType = null;
+    for(var a=0;a<infoCollections.length;a++){
+      switch(permission & parseInt(infoCollections[a],8)){
+        case 16384:
+          whatType = 'directory';
+          break;
+        case 32768:
+          whatType = 'file';
+          break;
+        case 40960:
+          whatType = 'link';
+          break;
+      }
+      if(whatType != null){
+        break;
+      }
+    }
+    if(passTypeData!=null){
+      if(passTypeData == whatType){
+        return true;
+      }
+      return false;
+    }
+    return whatType;
+  },
+  _waitingTimeout : function(timeoutNumber){
+    return new Promise((resolve : Function)=>{
+      setTimeout(() => {
+        resolve();
+      }, timeoutNumber);
+    })
   }
 });
