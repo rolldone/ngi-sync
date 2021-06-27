210eeb815ea2cb78b38cae4a3f8695b2b72991b1
donny rolanda <donny.rolanda@gmail.com>
2021-06-28 06:20:52 +0800

    - use nodejs code to get home directory

--- app/directaccess/compute/DirectAccess.ts
+++ app/directaccess/compute/DirectAccess.ts
@@ -2,12 +2,14 @@ import { ConfigInterface } from "@root/app/init/compute/Config";
 import BaseModel, { BaseModelInterface } from "@root/base/BaseModel";
 import SSHConfig, { SSHConfigInterface } from "@root/tool/ssh-config";
 import { readFileSync, writeFileSync } from "fs";
+import upath from 'upath';
+import os from 'os';
 import * as child_process from 'child_process';
 
 export type DirectAccessType = {
   ssh_configs: Array<any>
   ssh_commands: Array<any>
-  config_file: string
+  // config_file: string
 }
 
 export interface DirectAccessInterface extends BaseModelInterface {
@@ -25,7 +27,9 @@ const DirectAccess = BaseModel.extend<Omit<DirectAccessInterface, 'model'>>({
   construct(config) {
     this._config = config;
     let _direct_access: DirectAccessType = this._config.direct_access as any;
-    this._ssh_config = SSHConfig.parse(readFileSync(_direct_access.config_file).toString());
+    // this._ssh_config = SSHConfig.parse(readFileSync(_direct_access.config_file).toString());
+    let _configFilePath = upath.normalizeSafe(os.homedir()+'/.ssh/config');
+    this._ssh_config = SSHConfig.parse(readFileSync(_configFilePath).toString());
     for (var a = 0; a < _direct_access.ssh_configs.length; a++) {
       var sshSection = this._ssh_config.find({ Host: _direct_access.ssh_configs[a].Host })
       if (sshSection != null) {
@@ -35,7 +39,7 @@ const DirectAccess = BaseModel.extend<Omit<DirectAccessInterface, 'model'>>({
     for (var a = 0; a < _direct_access.ssh_configs.length; a++) {
       this._ssh_config.append(_direct_access.ssh_configs[a]);
     }
-    writeFileSync(_direct_access.config_file, SSHConfig.stringify(this._ssh_config));
+    writeFileSync(_configFilePath, SSHConfig.stringify(this._ssh_config));
   },
   submitDirectAccess: function (_select_ssh_command) {
     var child = child_process.spawn(_select_ssh_command.command, [''], {
