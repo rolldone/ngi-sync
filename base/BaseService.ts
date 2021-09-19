6deef96444623dcc033a8203b6e252a79c3b123c
rolldone <donny.rollproject@gmail.com>
2021-09-19 20:48:22 +0800

    Improvement - Remove unused code and package lib

--- base/BaseService.ts
+++ base/BaseService.ts
@@ -1,10 +1,7 @@
-import { Moment, Validator } from "@root/tool";
+import { Moment } from "@root/tool";
 import BaseProto from "./BaseProto";
 
 export default BaseProto.extend<BaseServiceInterface>({
-  returnValidator(form_data,form_rule){
-    return new Validator(form_data,form_rule);
-  },
   returnMoment(){
     return Moment();
   }
--- tool/Express.ts
+++
@@ -1,7 +0,0 @@
-import Express, {Request, Response} from 'express';
-
-export type {
-  Request,
-  Response
-}
-export default Express();
