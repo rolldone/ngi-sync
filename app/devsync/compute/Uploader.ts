afa86bde848552e883e9decec2432167177edca7
rolldone <donny.rollproject@gmail.com>
2021-12-11 11:42:44 +0800

    - Improve force sync navigation

--- app/devsync/compute/Uploader.ts
+++ app/devsync/compute/Uploader.ts
@@ -179,7 +179,7 @@ export default class Uploader {
 						}
 
 						try {
-							await this.client.mkdir(upath.dirname(remote), true)
+							await this.client.mkdir(upath.dirname(remote), false)
 							await this.client.chmod(upath.dirname(remote), this.config.pathMode)
 						} catch (ex) {}
 
