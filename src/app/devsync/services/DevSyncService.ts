962dbc7ff386b2774cf8616cf837d3be4aa8cb1c
donny <donny.rolanda@gmail.com>
2022-07-11 08:32:06 +0800

    Improvement
    - [PrivateKey] : Add permission 400
    - [ForceRsync] : Ignore privatekey path

--- src/app/devsync/services/DevSyncService.ts
+++ src/app/devsync/services/DevSyncService.ts
@@ -635,7 +635,7 @@ const DevSyncService = BaseService.extend<DevSyncServiceInterface>({
 
             setTimeout(() => {
               this.construct(this._cli);
-            }, 1000);
+            }, 2000);
           }
           var closeRemote = () => {
             if (this._currentConf.devsync.script.remote.on_stop != "" && this._currentConf.devsync.script.remote.on_stop != null) {
