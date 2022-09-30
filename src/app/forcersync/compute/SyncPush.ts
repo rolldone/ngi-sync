c169b80084523b87660a3dc9e08f7fecaea07c6f
donny rolanda <donny.rolanda@gmail.com>
2022-09-30 13:45:53 +0800

    Improvement - [ForceRsync] : Add promise rsync cache - [LocalBash] : Prevent ctrl+C on emulate bash

--- src/app/forcersync/compute/SyncPush.ts
+++ src/app/forcersync/compute/SyncPush.ts
@@ -591,7 +591,7 @@ const SyncPush = BaseModel.extend<Omit<SyncPushInterface, 'model'>>({
           }
         });
 
-        ptyProcess.on('exit', (exitCode: any, signal: any) => {
+        ptyProcess.on('exit', async (exitCode: any, signal: any) => {
           // process.stdin.off('keypress', theCallback);
           ptyProcess.kill();
           ptyProcess = null;
@@ -600,7 +600,7 @@ const SyncPush = BaseModel.extend<Omit<SyncPushInterface, 'model'>>({
               this._recursiveRsync(extraWatchs, index, _is_file);
             } else {
               // Cache it to temp
-              this._cacheToTemp(extraWatchs, index, isFile);
+              await this._cacheToTemp(extraWatchs, index, isFile);
               // And next recursive
               this._recursiveRsync(extraWatchs, index + 1);
             }
@@ -610,7 +610,7 @@ const SyncPush = BaseModel.extend<Omit<SyncPushInterface, 'model'>>({
               return;
             }
             // Cache it to temp
-            this._cacheToTemp(extraWatchs, index, isFile);
+            await this._cacheToTemp(extraWatchs, index, isFile);
             // ANd exit
             this._onListener({
               action: "exit",
@@ -629,102 +629,106 @@ const SyncPush = BaseModel.extend<Omit<SyncPushInterface, 'model'>>({
     }
   },
   _cacheToTemp(extraWatchs, index = 0, isFile = false) {
-    try {
-      let config = this._config;
-      let _local_path = config.local_path;
-      let _is_file = false;
-      if (extraWatchs[index] != null) {
+    return new Promise((resolve: Function) => {
+      try {
+        let config = this._config;
+        let _local_path = config.local_path;
+        let _is_file = false;
+        if (extraWatchs[index] != null) {
 
-        _local_path = path.relative(upath.normalizeSafe(path.resolve("")), upath.normalizeSafe(_local_path + '/' + extraWatchs[index].path));
-        let _remote_path = extraWatchs[index].path;
-        if (isFile == true) {
-          /* Remove file path to be dirname only */
-          _local_path = path.relative(upath.normalizeSafe(path.resolve("")), upath.normalizeSafe(_local_path + '/' + dirname(extraWatchs[index].path)));
-          _local_path = upath.normalizeSafe('./' + _local_path);
-          _remote_path = dirname(_remote_path);
-        } else {
-          _local_path = upath.normalizeSafe('./' + _local_path + '/')
-        }
-
-        process.stdout.write(chalk.yellow('Rsync Upload Cache | ') + _local_path + ' >> ' + upath.normalize(this.tempFolder+"/"+_remote_path) + '\n');
-
-        let _delete_mode_active = config.mode == "hard" ? true : false;
-        _delete_mode_active = extraWatchs[index].includes.length > 0 ? false : _delete_mode_active
-        var rsync = Rsync.build({
-          /* Support multiple source too */
-          source: _local_path,
-          // source : upath.normalize(_local_path+'/'),
-          destination: upath.normalize(this.tempFolder + "/" + _remote_path),
-          /* Include First */
-          include: extraWatchs[index].includes,
-          /* Exclude after include */
-          exclude: extraWatchs[index].ignores,
-          // set: '--usermap=*:' + this._config.username + ' --groupmap=*:' + this._config.username + ' --chmod=D2775,F775 --size-only --checksum ' + (_delete_mode_active == true ? '--force --delete' : ''),
-          // flags : '-vt',
-          flags: '-avzLm',
-          // shell: 'ssh -i ' + config.privateKeyPath + ' -p ' + config.port
-        });
-
-        // console.log("rsync commandnya :: ", rsync.command());
-        // process.stdout.write(chalk.green('Rsync Upload Cache | ') + 'rsync command -> ' + rsync.command() + '\n');
-
-        var shell = os.platform() === 'win32' ? "C:\\Program Files\\Git\\bin\\bash.exe" : 'bash';
-        var ptyProcess = this.iniPtyProcess(shell, []);
-        if (_is_file == false) {
-          ptyProcess.write('ls ' + _local_path + ' ' + '\r');
-        }
-        setTimeout(() => {
-          if (ptyProcess != null) {
-            ptyProcess.write(rsync.command() + '\r');
+          _local_path = path.relative(upath.normalizeSafe(path.resolve("")), upath.normalizeSafe(_local_path + '/' + extraWatchs[index].path));
+          let _remote_path = extraWatchs[index].path;
+          if (isFile == true) {
+            /* Remove file path to be dirname only */
+            _local_path = path.relative(upath.normalizeSafe(path.resolve("")), upath.normalizeSafe(_local_path + '/' + dirname(extraWatchs[index].path)));
+            _local_path = upath.normalizeSafe('./' + _local_path);
+            _remote_path = dirname(_remote_path);
+          } else {
+            _local_path = upath.normalizeSafe('./' + _local_path + '/')
           }
-        }, 2000);
 
-        ptyProcess.on('data', (data: any) => {
-          // console.log(data)
-          // let _text = this._stripAnsi(data.toString());
-          let _split = data.split(/\n/);// this._stripAnsi(data.toString());
-          if (_split != "") {
-            for (var a = 0; a < _split.length; a++) {
-              switch (_split[a]) {
-                case '':
-                case '\r':
-                case '\u001b[32m\r':
-                  break;
-                default:
-                  break;
+          process.stdout.write(chalk.yellow('Rsync Upload Cache | ') + _local_path + ' >> ' + upath.normalize(this.tempFolder + "/" + _remote_path) + '\n');
+
+          let _delete_mode_active = config.mode == "hard" ? true : false;
+          _delete_mode_active = extraWatchs[index].includes.length > 0 ? false : _delete_mode_active
+          var rsync = Rsync.build({
+            /* Support multiple source too */
+            source: _local_path,
+            // source : upath.normalize(_local_path+'/'),
+            destination: upath.normalize(this.tempFolder + "/" + _remote_path),
+            /* Include First */
+            include: extraWatchs[index].includes,
+            /* Exclude after include */
+            exclude: extraWatchs[index].ignores,
+            // set: '--usermap=*:' + this._config.username + ' --groupmap=*:' + this._config.username + ' --chmod=D2775,F775 --size-only --checksum ' + (_delete_mode_active == true ? '--force --delete' : ''),
+            // flags : '-vt',
+            flags: '-avzLm',
+            // shell: 'ssh -i ' + config.privateKeyPath + ' -p ' + config.port
+          });
+
+          // console.log("rsync commandnya :: ", rsync.command());
+          // process.stdout.write(chalk.green('Rsync Upload Cache | ') + 'rsync command -> ' + rsync.command() + '\n');
+
+          var shell = os.platform() === 'win32' ? "C:\\Program Files\\Git\\bin\\bash.exe" : 'bash';
+          var ptyProcess = this.iniPtyProcess(shell, []);
+          if (_is_file == false) {
+            ptyProcess.write('ls ' + _local_path + ' ' + '\r');
+          }
+          setTimeout(() => {
+            if (ptyProcess != null) {
+              ptyProcess.write(rsync.command() + '\r');
+            }
+          }, 2000);
+
+          ptyProcess.on('data', (data: any) => {
+            // console.log(data)
+            // let _text = this._stripAnsi(data.toString());
+            let _split = data.split(/\n/);// this._stripAnsi(data.toString());
+            if (_split != "") {
+              for (var a = 0; a < _split.length; a++) {
+                switch (_split[a]) {
+                  case '':
+                  case '\r':
+                  case '\u001b[32m\r':
+                    break;
+                  default:
+                    break;
+                }
               }
             }
-          }
-          if (data.includes('Not a directory')) {
-            _is_file = true;
-            ptyProcess.write('exit' + '\r');
-          }
-        });
+            if (data.includes('Not a directory')) {
+              _is_file = true;
+              ptyProcess.write('exit' + '\r');
+            }
+          });
 
-        ptyProcess.on('exit', (exitCode: any, signal: any) => {
-          // process.stdin.off('keypress', theCallback);
-          ptyProcess.kill();
-          ptyProcess = null;
-          if (extraWatchs[index + 1] != null) {
-            if (_is_file == true) {
-              this._cacheToTemp(extraWatchs, index, _is_file);
+          ptyProcess.on('exit', (exitCode: any, signal: any) => {
+            // process.stdin.off('keypress', theCallback);
+            ptyProcess.kill();
+            ptyProcess = null;
+            if (extraWatchs[index + 1] != null) {
+              if (_is_file == true) {
+                this._cacheToTemp(extraWatchs, index, _is_file);
+              } else {
+                // You dont need it
+                // this._cacheToTemp(extraWatchs, index + 1);
+              resolve();
+              }
             } else {
-              // You dont need it
-              // this._cacheToTemp(extraWatchs, index + 1);
+              if (_is_file == true) {
+                this._cacheToTemp(extraWatchs, index, _is_file);
+                return;
+              }
+              resolve();
             }
-          } else {
-            if (_is_file == true) {
-              this._cacheToTemp(extraWatchs, index, _is_file);
-              return;
-            }
-          }
-        });
+          });
 
-        // recursive();
+          // recursive();
+        }
+      } catch (ex) {
+        console.log('_recursiveRsync - ex ', ex);
       }
-    } catch (ex) {
-      console.log('_recursiveRsync - ex ', ex);
-    }
+    });
   },
   submitPush: async function () {
     try {
